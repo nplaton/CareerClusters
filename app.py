c27f072e47b22b38452918d07494a6ab69c13c27
Zipfian Academy <Zipfian@Zipfians-Mac-mini-6.local>
2014-11-06 11:33:21 -0800

    Web app recommender working

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask import request
+import pandas as pd
+import numpy as np
 import pickle
 import ipdb
+from scipy import spatial
+
 app = Flask(__name__)
  
 fit_model = pickle.load(open( 'data/fitted_model.pkl', 'rb' ))
 fit_vectorizer = pickle.load(open( 'data/fitted_vectorizer.pkl', 'rb' ))
+vector_matrix = pickle.load(open( 'data/vector_matrix.pkl', 'rb' ))
+df = pd.read_csv('data/SOpostings.csv')
 
 # home page
 @app.route('/')
@@ -27,12 +33,22 @@ def classifier():
     # get data from request form, the key is the name you set in your form
     data = str(request.form['user_input'])
     vectorize_data = fit_vectorizer.transform([data])
-    guess = fit_model.predict(vectorize_data)
+    # guess = fit_model.predict(vectorize_data)
     # ipdb.set_trace()
-    returner = ''' Category: goes here <br> <a href="/"> Back to home page </a>''' 
+    cos = []
+    transformed = vectorize_data.T.toarray()
+    vec_array = vector_matrix.toarray()
+    for x in range(vec_array.shape[0]):
+        cos.append(spatial.distance.cosine(vec_array[x,:], transformed))
+    # for x in np.array(cos).argsort()[:10]:
+    #     df.links[x]
+    x = np.array(cos).argsort()[0]
+
+
+    returner = ''' Top job for you: <br> <a href="%s" target="_blank"> %s </a>  <br> <a href="/"> Back to home page </a>''' 
     
     # now return your results 
-    return  returner
+    return  returner % (df.links[x], df.titles[x])
 
 # fit_model.close()
 # fit_vectorizer.close()
--- build_model.py
+++ build_model.py
@@ -8,10 +8,11 @@ df = pd.read_csv('data/SOpostings.csv')
 desc = df.descriptions
 
 vectorizer = TfidfVectorizer(stop_words = 'english', min_df=1)
-vectorized_matrix = vectorizer.fit_transform(desc)
+vector_matrix = vectorizer.fit_transform(desc)
 
 km = KMeans(7)
-km.fit(vectorized_matrix)
+km.fit(vector_matrix)
 
 pickle.dump(km, open('data/fitted_model.pkl', 'wb'))
 pickle.dump(vectorizer, open('data/fitted_vectorizer.pkl', 'wb'))
+pickle.dump(vector_matrix, open('data/vector_matrix.pkl', 'wb'))
