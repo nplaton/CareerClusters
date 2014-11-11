57a4f1801142cd4d5492e7f1a40a554294e2b1d1
Zipfian Academy <Zipfian@Zipfians-Mac-mini-6.local>
2014-11-11 11:35:43 -0800

    Working resume optimize chart

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import pickle
 import ipdb
 from scipy import spatial
 import re
+import resume_optimize
+import json
 
 app = Flask(__name__)
  
@@ -37,15 +39,21 @@ def classifier():
     x = np.array(cos).argsort()[:20]
     returner_df = df.ix[x,:]
     data = returner_df.values
-
-    # returner = ''' Top job for you: <br> <a href="%s" target="_blank"> %s </a>''' 
-    # returner = returner_df.to_html()
     
     # now return your results 
-    return  render_template('index.html', data=data)
+    return render_template('job_postings.html', data=data)
 
-# fit_model.close()
-# fit_vectorizer.close()
+@app.route('/optimize_resume')
+def optimize_resume():
+    return render_template('resume_optimize.html')
+
+@app.route('/improve_resume', methods=['POST'] )
+def improve_resume():
+    job_posting = re.sub('[^\w\s]+', '', request.form['jd']).encode('utf-8')
+    print job_posting
+    cv = re.sub('[^\w\s]+', '', request.form['cv']).encode('utf-8')
+    data = resume_optimize.make_posting_to_dict(job_posting, cv)
+    return render_template('resume_chart.html', data=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6969, debug=True)
--- resume_optimize.py
+++ resume_optimize.py
@@ -1,32 +1,31 @@
 from string import punctuation
 from collections import Counter
 
-basic_list_of_dict = [ {'key': 'Job Posting',
-  'values': [],
-  'yAxis': '1'},
-  {'key': 'Your Resume',
-  'values': [],
-  'yAxis': '1'}]
-
-all_skills = ['python', 'scikit-learn', 'scipy', 'sympy', 'nltk', 'pymc', 'pandas', 'numpy', 'beautifulsoup',
- 'matlab', 'mongodb', 'sql', 'postgres', 'sqlite', 'visualization', 'matplotlib', 'd3.js', 'gephi',
- 'graphlab', 'non-linear', 'optimization', 'algorithms', 'numerical', 'ordinary', 'partial', 'differential',
- 'equations', 'sympy', 'numpy', 'machine', 'learning', 'sympy', 'numpy', 'machine', 'learning', 'sympy',
- 'numpy', 'machine', 'learning', 'unix', 'perl', 'r', 'math', 'statistics', 'phd', 'masters', 'ruby', 'rails',
- 'front', 'end', 'database', 'data', 'web', 'development']
+all_skills = ["python", "scikit-learn", "scipy", "sympy", "nltk", "pymc", "pandas", "numpy", "beautifulsoup",
+ "matlab", "mongodb", "sql", "postgres", "sqlite", "visualization", "matplotlib", "d3.js", "gephi",
+ "graphlab", "non-linear", "optimization", "algorithms", "numerical", "ordinary", "partial", "differential",
+ "equations", "sympy", "numpy", "machine", "learning", "sympy", "numpy", "machine", "learning", "sympy",
+ "numpy", "machine", "learning", "unix", "perl", "r", "math", "statistics", "phd", "masters", "ruby", "rails",
+ "front", "end", "database", "data", "web", "development", "ios", "android", "mobile"]
 
 def get_clean_skills(raw_text):
     x = [x.strip(punctuation).lower() for x in raw_text.split()]
-    return [y for y in x if y in allskills]
+    return [y for y in x if y in all_skills]
 
 def make_counter(raw_text):
     clean_text = get_clean_skills(raw_text)
     return Counter(clean_text)
 
 def make_posting_to_dict(raw_posting, raw_resume):
+    basic_list_of_dict = [{"key": "Job Posting",
+    "values": [],
+    "yAxis": "1"},
+    {"key": "Your Resume",
+    "values": [],
+    "yAxis": "1"}]
     for x, y in make_counter(raw_posting).iteritems():
-        basic_list_of_dict[0]['values'].append({'x': x, 'y' : y})
+        basic_list_of_dict[0]["values"].append({"x": x, "y" : y})
     subtract = {key: make_counter(raw_resume).get(key, 0) for key in make_counter(raw_posting).keys()}
     for x, y in subtract.iteritems():
-        basic_list_of_dict[1]['values'].append({'x': x, 'y' : y})
+        basic_list_of_dict[1]["values"].append({"x": x, "y" : y})
     return basic_list_of_dict
