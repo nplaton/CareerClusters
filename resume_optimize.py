ebb98167b7656fe1e30b24dcd38e933fcba642f2
Zipfian Academy <Zipfian@Zipfians-Mac-mini-6.local>
2014-11-10 19:53:10 -0800

    Added function to build viz dict

--- resume_optimize.py
+++ resume_optimize.py
@@ -1,6 +1,20 @@
 from string import punctuation
 from collections import Counter
 
+basic_list_of_dict = [ {'key': 'Job Posting',
+  'values': [],
+  'yAxis': '1'},
+  {'key': 'Your Resume',
+  'values': [],
+  'yAxis': '1'}]
+
+all_skills = ['python', 'scikit-learn', 'scipy', 'sympy', 'nltk', 'pymc', 'pandas', 'numpy', 'beautifulsoup',
+ 'matlab', 'mongodb', 'sql', 'postgres', 'sqlite', 'visualization', 'matplotlib', 'd3.js', 'gephi',
+ 'graphlab', 'non-linear', 'optimization', 'algorithms', 'numerical', 'ordinary', 'partial', 'differential',
+ 'equations', 'sympy', 'numpy', 'machine', 'learning', 'sympy', 'numpy', 'machine', 'learning', 'sympy',
+ 'numpy', 'machine', 'learning', 'unix', 'perl', 'r', 'math', 'statistics', 'phd', 'masters', 'ruby', 'rails',
+ 'front', 'end', 'database', 'data', 'web', 'development']
+
 def get_clean_skills(raw_text):
     x = [x.strip(punctuation).lower() for x in raw_text.split()]
     return [y for y in x if y in allskills]
@@ -9,7 +23,10 @@ def make_counter(raw_text):
     clean_text = get_clean_skills(raw_text)
     return Counter(clean_text)
 
-def find_matched_words(job_posting, resume_post):
-    job_posting_dict = make_counter(job_posting)
-    resume_dict = make_counter(resume_post)
-    return {key: job_posting_dict[key] - resume_dict.get(key, 0) for key in job_posting_dict.keys()}
+def make_posting_to_dict(raw_posting, raw_resume):
+    for x, y in make_counter(raw_posting).iteritems():
+        basic_list_of_dict[0]['values'].append({'x': x, 'y' : y})
+    subtract = {key: make_counter(raw_resume).get(key, 0) for key in make_counter(raw_posting).keys()}
+    for x, y in subtract.iteritems():
+        basic_list_of_dict[1]['values'].append({'x': x, 'y' : y})
+    return basic_list_of_dict
